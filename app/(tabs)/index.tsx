9970593e831f50fb96aa543063842164212f6182
igudy <goodnessigunma1@gmail.com>
2025-06-06 14:55:35 +0100

    fix: initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,53 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.reactLogo}
+        />
+      }
+    >
+      <View className="bg-slate-100 rounded-xl">
+        <Text className="text-lg font-medium text-white">Welcome to Tailwind</Text>
+      </View>
+
+      <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white text-2xl font-bold">Hello, Tailwind!</Text>
+    </View>
+
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Welcome my bro</ThemedText>
+
+        <HelloWave />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
